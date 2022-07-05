edf38ed2f31b7f2e8e14e568b88564f01dc9aa46
Jędrzej Runowicz <jedrzej.runowicz@gmail.com>
2022-07-05 21:46:12 +0200

    Forms update

--- mainapp/posts/models.py
+++ mainapp/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.CharField(max_length=170)
+
+    def __str__(self):
+        return self.text
--- mainapp/posts/views.py
+++ mainapp/posts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import PostForm
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            post.user = request.user
+            post.save()
+            return render(request, 'posts/posts_index.html')
+    else:
+        form = PostForm()
+
+    context = {'form': form}
+    return render(request, 'posts/posts_index.html', context)
