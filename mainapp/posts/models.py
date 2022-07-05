1d68576f713067d6595fb3b1fb68ea2b952d44b9
Jędrzej Runowicz <jedrzej.runowicz@gmail.com>
2022-07-05 23:47:42 +0200

    Form changes

--- mainapp/posts/models.py
+++ mainapp/posts/models.py
@@ -5,6 +5,3 @@ from django.conf import settings
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.CharField(max_length=170)
-
-    def __str__(self):
-        return self.text
--- mainapp/posts/views.py
+++ mainapp/posts/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from .forms import PostForm
-from django.shortcuts import redirect
 
 
 def index(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save()
-            post.user = request.user
-            post.save()
+            form = form.save(commit=False)
+            form.user = request.user
+            form.save()
             return render(request, 'posts/posts_index.html')
     else:
         form = PostForm()
