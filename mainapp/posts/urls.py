8fce2af976d8180902050546caebffc7148360f6
Jędrzej Runowicz <j.runowicz@tkhtechnology.com>
2022-07-06 13:05:34 +0200

    Posts and design upadte

--- mainapp/posts/urls.py
+++ mainapp/posts/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='posts'),
-    path('', views.posts, name='posts'),
 ]
--- mainapp/posts/views.py
+++ mainapp/posts/views.py
@@ -10,17 +10,16 @@ def index(request):
         form = PostForm(request.POST)
         if form.is_valid():
             form = form.save(commit=False)
-            form.user = request.user
+            form.user = request.user.id
             form.save()
             return render(request, 'posts/posts_index.html')
     else:
         form = PostForm()
 
-    context = {'form': form}
-    return render(request, 'posts/posts_index.html', context)
-
-
-def posts(request):
     list_of_posts = ModelPost.objects.all().values()
-    context = {'list_of_posts': list_of_posts}
+
+    context = {
+        'form': form,
+        'list_of_posts': list_of_posts
+    }
     return render(request, 'posts/posts_index.html', context)
